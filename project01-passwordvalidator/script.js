ecc3eb8bb8cf8bd6564cc091ea9577802714f8f8
shishirkhatiwada <58501996+shishirkhatiwada@users.noreply.github.com>
2023-09-20 11:56:19 +0000

    slash added when clicked on icon

--- project01-passwordvalidator/script.js
+++ project01-passwordvalidator/script.js
@@ -3,6 +3,13 @@ const eyeIcons = document.querySelectorAll(".password-field i.fa-eye");
 
 eyeIcons.forEach((eyeIcon) => {
     eyeIcon.addEventListener("click", () => {
-        passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+       
+        //show and unshow slash 
+
+    eyeIcon.className = `fa-solid fa-eye${passwordInput === "password" ? "" : "-slash"}`
+
     });
+
+   
 });
